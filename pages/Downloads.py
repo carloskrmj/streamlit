81d44630b1e0950a96b86f4ee51a3a3cf408a48e
carloskrmj <150045215+carloskrmj@users.noreply.github.com>
2024-10-18 15:26:41 -0300

    Update Downloads.py

--- pages/Downloads.py
+++ pages/Downloads.py
@@ -1,14 +1,32 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
-st.set_page_config(page_title="Downloads", layout="wide")
+st.set_page_config(page_title="Transparência e Downloads", layout="wide")
 
 # Título da página
-st.title("Página de Downloads")
+st.title("Transparência e Downloads")
 
-# Conteúdo da página de downloads
-st.write("Em construção")
+st.header("NF's de parcerias publicas")
 
-# Botão para voltar à página principal
-if st.button("Voltar para a Página Principal"):
-    switch_page("app")
+# Carregar o arquivo PDF
+with open("assets/documentos/35503082229899331000197000000000000824065208566351.pdf", "rb") as pdf_file:
+    danfe_12062024 = pdf_file.read()
+
+with open("assets/documentos/35503082229899331000197000000000000924076481969951.pdf", "rb") as pdf_file:
+    danfe_02072024 = pdf_file.read()
+
+# Criação do botão de download para o PDF
+st.download_button(
+    label="Baixar PDF - danfe_12-06-2024",
+    data=danfe_12062024,
+    file_name="danfe_12-06-2024.pdf",
+    mime="application/pdf"
+)
+
+# Criação do botão de download para o PDF
+st.download_button(
+    label="Baixar PDF - danfe_02-07-2024",
+    data=danfe_02072024,
+    file_name="danfe_02-07-2024.pdf",
+    mime="application/pdf"
+)
