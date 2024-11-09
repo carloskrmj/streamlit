6a72a3728bae9ef06c1dee5d18fec3123444d004
carloskrmj <150045215+carloskrmj@users.noreply.github.com>
2024-11-08 21:36:33 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
+
 # Configurações da página
 st.set_page_config(page_title="Projeto Social T3", layout="wide")
 
@@ -29,12 +30,34 @@ st.markdown(
 st.markdown('<h1 class="title">Bem-vindo à Associação Desportiva T3</h1>', unsafe_allow_html=True)
 st.write('---')
 
-col1, col2, col3 = st.columns([2,3,3])
-with col2:
-    # Inserir a imagem logo abaixo do separador
-    st.image("assets/favpng_black-belt-judo-dan-brazilian-jiu-jitsu.png", caption="BJJ", width=400)
 
+left_co, cent_co, right_co = st.columns([1, 10, 1])
+
+
+
+with cent_co:
+    # Inserir a imagem logo abaixo do separador
+    st.image("assets/lgbt-banner-11-2024.png", width=830)
+
+
+# centered
+st.markdown(
+    """<h3><a style='display: block; text-align: center;' href="https://forms.gle/ySKFH2KJaZnXaHnA8">4º JOGOS LGBTQIAPN+ - INSCRIÇÃO 2024</a></h3>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    """<h4>Nos dias 23, 24, 30 de novembro e 01 de dezembro acontecerá no Centro Olímpico de Treinamento e Pesquisa (Voleibol, Handebol e Futsal) e no CERET (Rugby) a 4ª edição dos Jogos LGBTQIAPN+.</h4>
+    """,
+    unsafe_allow_html=True,
+)
 st.markdown('</div>', unsafe_allow_html=True)
 
-# Conteúdo da página principal
-st.write("Em construção")
+st.markdown(
+    """<h5><a style='display: block; text-align: center;' href="https://forms.gle/ySKFH2KJaZnXaHnA8">INSCREVA-SE AQUI</a></h5>
+    """,
+    unsafe_allow_html=True,
+)
+
+
